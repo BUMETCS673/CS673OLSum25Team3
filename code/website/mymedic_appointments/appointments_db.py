398bc2b3d1d3aa060b15e4a2585337aac917c33d
Adriel Domingo <adrieldomingo@gmail.com>
2025-06-07 14:14:29 -0400

    add django testing

--- code/website/mymedic_appointments/appointments_db.py
+++ code/website/mymedic_appointments/appointments_db.py
@@ -1,9 +1,9 @@
 import sqlite3
 from appointments import Appointment
 
-conn = sqlite3.connect(':memory:')
+# conn = sqlite3.connect(':memory:')
 
-# conn = sqlite3.connect('appointments.db')
+conn = sqlite3.connect('appointments.db')
 
 c = conn.cursor()
 
@@ -16,29 +16,40 @@ c.execute("""CREATE TABLE appointments (
             status text
             )""")
 
+
 # add appointment
 def add_apt(apt):
     with conn:
-        pass
+        c.execute("INSERT INTO appointments VALUES (:id, :pid, :did, :dt, "
+                  ":reason, :stat)",
+                  {'id': apt.id, 'pid': apt.patient_id, 'did': apt.doctor_id,
+                   'dt': apt.datetime, 'reason': apt.reason,
+                   'stat': apt.status})
 
 
 # remove appointment
 def rem_apt(apt):
     with conn:
-        pass
+        c.execute("DELETE from appointments WHERE id = :id",
+                  {'id': apt.id})
 
 
 # update appointment time
 def update_time(apt, time):
     with conn:
-        pass
+        c.execute("UPDATE appointments SET datetime = :dt WHERE id = :id",
+                  {'dt': time, 'id': apt.id})
 
 
 # get appointment by patient_id
 def get_apt_patient(patient_id):
-    pass
+    c.execute("SELECT * FROM appointments WHERE patient_id =:pid",
+              {'pid': patient_id})
+    return c.fetchall()
 
 
 # get appointment by doctor_id
 def get_apt_doctor(doctor_id):
-    pass
+    c.execute("SELECT * FROM appointments WHERE doctor_id =:did",
+              {'did': doctor_id})
+    return c.fetchall()
