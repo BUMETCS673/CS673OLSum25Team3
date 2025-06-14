f59f240db54715e3f65d00dbe22dc12b5ea774e6
uzayisinalici <isinalc@gmail.com>
2025-06-14 11:20:50 +0300

    Revert "Moved sensitive email settings to .env"
    This reverts commit 678e863fed4390465bc681500cfdd2e6b3d56410.

--- code/mymedic/mymedic/settings.py
+++ code/mymedic/mymedic/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,9 +127,9 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mymedic.app01@gmail.com'
+EMAIL_HOST_PASSWORD = 'whnj vqbl jjsa qskd'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
