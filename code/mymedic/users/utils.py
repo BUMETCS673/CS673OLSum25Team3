d7bab73bef09f07766afb121e8599e43796f6feb
uzayisinalici <isinalc@gmail.com>
2025-06-15 21:40:55 +0300

    fixed settings.py

--- code/mymedic/users/utils.py
+++ code/mymedic/users/utils.py
@@ -2,21 +2,21 @@ import random
 from django.core.mail import send_mail
 from django.conf import settings
 
-def generate_mfa_code():
-    return str(random.randint(100000, 999999))
-
 def send_mfa_code(user_email, request):
-    code = generate_mfa_code()
-    print("Sending MFA code:", code)
-
-    result = send_mail(
-        subject="Your MyMedic MFA Code",
-        message=f"Your verification code is: {code}",
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user_email],
-        fail_silently=False
-    )
-
-    print("Was the email sent?:", result)
-
+    code = str(random.randint(100000, 999999))
     request.session['mfa_code'] = code
+
+    print("Generated MFA Code:", code)
+
+    try:
+        result = send_mail(
+            subject="Your MyMedic MFA Code",
+            message=f"Your verification code is: {code}",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user_email],
+            fail_silently=False
+        )
+        print("Email send result (1=success):", result)
+
+    except Exception as e:
+        print("Failed to send MFA email:", e)
